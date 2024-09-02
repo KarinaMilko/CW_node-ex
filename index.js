c0fc4191c0be714a5ee0e0f89895cbefe73c1cc9
Karina <milkokarina13@gmail.com>
2024-09-02 10:29:50 +0300

    Add simple server

--- index.js
+++ index.js
@@ -1,55 +1,14 @@
-// ~ def imp
-// const sum = require('./math.js'); // module.exports
-
-// console.log(sum(1, 2));
-
-// ~ named imp
-// const Math = require('./math.js');
-
-// console.log(Math.sum(10, 20));
-// console.log(Math.mult(10, 20));
-
 // req
-// const { sum, mult } = require("./math.js");
-// console.log(sum(10, 20));
-// console.log(mult(10, 20));
+const http = require("node:http");
 
-const os = require("node:os");
-const fs = require("node:fs");
-const fsPromises = require("node:fs/promises");
-const path = require("node:path");
-
-console.log(os.cpus());
-console.log(os.version());
-
-console.log(global);
-console.log(process.env); // змінні оточення
-
-console.log(__dirname);
-console.log(__filename);
-
-// sync
-try {
-  const file = fs.readFileSync("./math.js", { encoding: "utf-8" });
-  console.log(file);
-} catch (err) {
-  console.log(err);
-}
-
-// async callback
-// error first
-fs.readFile("./math.js", { encoding: "utf-8" }, (err, file) => {
-  if (err) {
-    return console.log("err :>> ", err);
-  }
-  console.log("file :>> ", file);
+const server = http.createServer((request, response) => {
+  console.log("request.url :>> ", request.url);
+  console.log("request.method :>> ", request.method);
+  response.statusCode = 200;
+  response.end("Response");
 });
 
-//async promises
-fsPromises
-  .readFile("./math.js", { encoding: "utf-8" })
-  .then((file) => console.log(file))
-  .catch((err) => console.log(err));
-
-// path.join() - формує шлях
-// path.resolve() - формує абсолютний шлях
+// localhost = 127.0.0.1
+server.listen(5000, "127.0.0.1", () => {
+  console.log("Server is listening http://127.0.0.1:5000");
+});
